b79018a126fc4555e14a099974b4b1398386ab0c
AnikaBushra <anikabushra024@gmail.com>
2023-05-05 20:33:12 +0600

    vercel config added

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 const chefData = require('./data/chefdata.json')
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
+
 app.get('/chefdata', (req, res) => {
     res.send(chefData)
+})
+
+app.get('/chefdata/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const singleData = chefData.find(chef => parseInt(chef.id) === id);
+    res.send(singleData);
 })
 
 app.listen(port, () => {
